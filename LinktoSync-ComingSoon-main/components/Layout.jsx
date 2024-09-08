4f5eb00f9aae60f5f5aa3730ed6278be74a23123
Nivasika A <nivasika24@gmail.com>
2024-09-08 18:44:58 +0530

    Add files via upload

---
+++ LinktoSync-ComingSoon-main/components/Layout.jsx
@@ -0,0 +1,21 @@
+import Head from "next/head"
+import Footer from "./ui/Footer"
+import Navbar from "./ui/Navbar"
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <Head>
+                <title>LinktoSync</title>
+                <meta name='description' content="LinktoSync.com is a service that allows users to connect and post on their social media accounts from a Single Platform." />
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
---
+++ LinktoSync-ComingSoon-main/pages/index.js
@@ -0,0 +1,17 @@
+
+
+import Hero from "@/components/ui/Hero";
+
+import Testimonial from "@/components/ui/Testimonial";
+
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+
+      <Testimonial />
+     
+    </>
+  );
+}
